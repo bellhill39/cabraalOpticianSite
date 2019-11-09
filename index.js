f8835d088f95b650a2e528c219a15377a88be8f4
rashindasandaru.hal <playyboyy39@gmail.com>
2019-11-09 19:02:19 +0530

    updated the product line

--- index.js
+++ index.js
@@ -38,15 +38,13 @@ $(".nav-contact").click(function(){
   },2000);
 });
 
+//intro div view
+$("#intro").animate({opacity:1},{duration:4000});
+//product div view
+$("#products").animate({opacity:1},{duration:5000});
 //readme button
 
-// $(".buttonabout").click(function(){
-//   $("#aboutUs h1, .aboutUs-box").fadeToggle(2000);
-//   $(".storyAboutUs").fadeToggle(2000);
-//   $('html , body ').animate({
-//     scrollTop : $("#aboutUs").offset().top
-//   },2000);
-// });
+
 
 $(".buttonabout").click(function(){
   $(".aboutUs-box").fadeToggle(2000);
@@ -54,4 +52,37 @@ $(".buttonabout").click(function(){
   $('html , body ').animate({
     scrollTop : $("#aboutUs").offset().top
   },2000);
+});
+
+// sunglass image transfer
+
+$(".bgimage1").mouseenter(function (){
+  $(this).attr("id","bgImageHide");
+  $(".specS").hide();
+  $(".sunG").removeClass("col-md-6");
+  $(".sunG").addClass("col-md-12");
+});
+
+$(".bgimage1").mouseleave(function(){
+  $(this).removeAttr("id");
+  $(".specS").show();
+  $(".sunG").removeClass("col-md-12");
+  $(".sunG").addClass("col-md-6");
+});
+
+//spectacles image transfer
+
+$(".bgimage2").mouseenter(function(){
+  $(this).attr("id","bgImageHide");
+  $(".sunG").hide();
+  $(".specS").removeClass("col-md-6");
+  $(".specS").addClass("col-md-12");
+});
+
+$(".bgimage2").mouseleave(function(){
+  $(this).removeAttr("id");
+  $(".sunG").show();
+  $(".specS").removeClass("col-md-12");
+  $(".specS").addClass("col-md-6");
+
 });
