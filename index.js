a76dbda40907c4fd516a629b822e242e130426f2
rashindasandaru.hal <playyboyy39@gmail.com>
2019-11-11 00:50:06 +0530

    product logo add

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ $(".nav-contact").click(function(){
 //intro div view
 $("#intro").animate({opacity:1},{duration:4000});
 //product div view
-$("#products").animate({opacity:1},{duration:5000});
+$("#products").animate({opacity:1},{duration:2000});
 //readme button
 
 
@@ -61,6 +61,9 @@ $(".bgimage1").mouseenter(function (){
   $(".specS").hide();
   $(".sunG").removeClass("col-md-6");
   $(".sunG").addClass("col-md-12");
+  $(".sunBrand").animate({opacity:1},{duration:200});
+  $(".brandCase").css({"background":"white"});
+  // $(".sunglassesBrands").show();
 });
 
 $(".bgimage1").mouseleave(function(){
@@ -68,21 +71,63 @@ $(".bgimage1").mouseleave(function(){
   $(".specS").show();
   $(".sunG").removeClass("col-md-12");
   $(".sunG").addClass("col-md-6");
+  // $(".sunglassesBrands").hide();
+  $(".sunBrand").animate({opacity:0},{duration:100});
+  $(".brandCase").css({"background":"none"});
 });
 
 //spectacles image transfer
 
 $(".bgimage2").mouseenter(function(){
-  $(this).attr("id","bgImageHide");
+  $(this).attr("id","bgImageHide").css({"background-color":"white"});
   $(".sunG").hide();
   $(".specS").removeClass("col-md-6");
   $(".specS").addClass("col-md-12");
+  $(".specsBrand").animate({opacity:1},{duration:200});
 });
 
 $(".bgimage2").mouseleave(function(){
-  $(this).removeAttr("id");
+  $(this).removeAttr("id").css({"background-color":"#EFEFEF"});
   $(".sunG").show();
   $(".specS").removeClass("col-md-12");
   $(".specS").addClass("col-md-6");
+  $(".specsBrand").animate({opacity:0},{duration:100});
+});
 
+
+// Contact Lenses transfer
+
+$(".bgimage3").mouseenter(function(){
+  $(this).attr("id","bgImageHide");
+  $(".hearinG").hide();
+  $(".contacT").removeClass("col-md-6");
+  $(".contacT").addClass("col-md-12");
+  $(".contactBrand").animate({opacity:1},{duration:200});
+});
+
+$(".bgimage3").mouseleave(function(){
+  $(this).removeAttr("id");
+  $(".hearinG").show();
+  $(".contacT").removeClass("col-md-12");
+  $(".contacT").addClass("col-md-6");
+  $(".contactBrand").animate({opacity:0},{duration:100});
+});
+
+//hearing transfer
+
+$(".bgimage4").mouseenter(function(){
+  $(this).attr("id","bgImageHide").removeClass("text-white").addClass("text-black");
+  $(".contacT").hide();
+  $(".hearinG").removeClass("col-md-6");
+  $(".hearinG").addClass("col-md-12");
+  $(".hearingBrand").animate({opacity:1},{duration:200});
+
+});
+
+$(".bgimage4").mouseleave(function(){
+  $(this).removeAttr("id").removeClass("text-black").addClass("text-white");
+  $(".contacT").show();
+  $(".hearinG").removeClass("col-md-12");
+  $(".hearinG").addClass("col-md-6");
+  $(".hearingBrand").animate({opacity:0},{duration:100});
 });
